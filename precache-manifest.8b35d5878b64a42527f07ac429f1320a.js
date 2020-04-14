b4842cc3c6002ba25af8ecf997e50d34ce675731
Tanwinder <m_702677@macltus42973.attlocal.net>
2020-04-14 14:17:30 -0500

    Updates

---
+++ precache-manifest.8b35d5878b64a42527f07ac429f1320a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d58f19618628aaeee5f09c7776f8cd6",
+    "url": "/Tanwinder/react-hooks/index.html"
+  },
+  {
+    "revision": "7e31711f8b9851b3d0bf",
+    "url": "/Tanwinder/react-hooks/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "74093713b8067bdaf7ab",
+    "url": "/Tanwinder/react-hooks/static/js/2.531a95bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tanwinder/react-hooks/static/js/2.531a95bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e31711f8b9851b3d0bf",
+    "url": "/Tanwinder/react-hooks/static/js/main.ac9b4887.chunk.js"
+  },
+  {
+    "revision": "252dd28612532f34727f",
+    "url": "/Tanwinder/react-hooks/static/js/runtime-main.5b9ab69b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Tanwinder/react-hooks/static/media/logo.5d5d9eef.svg"
+  }
+]);
